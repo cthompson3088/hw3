9afc95269ab98ddcfb1d01efb7246eb845192c91
Collin Thompson <collin.thompson@kellogg.northwestern.edu>
2024-05-15 00:27:19 +0000

    checkpoint on day 2

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,24 @@
 class EntriesController < ApplicationController
-  def index
+
+  def new
+    @place = Place.find_by({"id" => params["place_id"]})
   end
+
+  def create 
+    @entry = Entry.new
+    @entry["title"]=params["title"]
+    @entry["posted_on"]=params["posted_on"]
+    @entry["description"]=params["description"]
+    
+    @entry["place_id"]=params["place_id"]
+    
+    @entry.save
+    redirect_to "/places/#{@entry["place_id"]}"
+  end
+
+
+
+
+
+
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,10 +2,23 @@ class PlacesController < ApplicationController
   def index
     @places = Place.all
   end
-end
 
 
-def show
-  @places = Place.find_by({"id" => params["id"]})
-  @entries = Entry.where({"place_id" => params["id"]})
+
+  def show
+    @place = Place.find_by({"id" => params["id"]})
+    @entries = Entry.where({"place_id" => @place["id"]})
+  end
+
+
+  def new
+  end
+
+
+  def create
+    @place = Place.new
+    @place["name"] = params["name"]
+    @place.save
+    redirect_to "/places"
+  end
 end
